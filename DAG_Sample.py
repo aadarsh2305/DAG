f7f8f659ccac76d09ed0e9f7742e5f7c65623fb0
Aadarsh <Aadarsh.Gupta@Prolifics.com>
2024-12-10 18:58:04 +0530

    Initial commit

---
+++ DAG_Sample.py
@@ -0,0 +1,74 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+import os
+from nbconvert import ScriptExporter
+from nbformat import read
+
+# Base path for your PySpark job notebooks and temporary converted Python scripts
+# NOTEBOOKS_DIR = "../../pyspark_notebooks"
+PY_SCRIPTS_DIR = "/home/innv1admn/migrated_scripts"
+
+# Function to convert notebooks to Python scripts
+def convert_notebook_to_py(notebook_path, output_dir):
+    with open(notebook_path, 'r', encoding='utf-8') as f:
+        nb = read(f, as_version=4)
+
+    exporter = ScriptExporter()
+    script, _ = exporter.from_notebook_node(nb)
+
+    base_name = os.path.basename(notebook_path).replace('.ipynb', '.py')
+    output_path = os.path.join(output_dir, base_name)
+
+    with open(output_path, 'w', encoding='utf-8') as f:
+        f.write(script)
+
+    return output_path
+
+# Convert the specific notebook files
+os.makedirs(PY_SCRIPTS_DIR, exist_ok=True)
+
+notebook_1_path = "/home/innv1admn/pyspark_notebooks/DailyPPMGMA.ipynb"
+notebook_2_path = "/home/innv1admn/pyspark_notebooks/talendtopysparkgit.ipynb"
+
+
+script_1 = convert_notebook_to_py(notebook_1_path, PY_SCRIPTS_DIR)
+script_2 = convert_notebook_to_py(notebook_2_path, PY_SCRIPTS_DIR)
+
+
+dag = DAG(
+    dag_id = 'PPM_DAG',
+    dag_display_name  = 'PPM DAG',
+    description='DAG to run migrated ppm pyspark jobs',
+    schedule_interval=None,  # Trigger manually
+    start_date=days_ago(1),
+    catchup=False,
+    tags = ['PPM','PySpark']
+)
+
+start = DummyOperator(
+    task_id='start',
+    dag=dag,
+)
+
+task1 = BashOperator(
+    task_id="AccountSync",
+    bash_command=f"spark-submit --conf spark.ui.port=5200 --jars /usr/share/java/mysql-connector-java-9.1.0.jar {script_2}",
+    dag=dag,
+)
+
+task2 = BashOperator(
+    task_id="DailyPPMGMA",
+    bash_command=f"spark-submit --conf spark.ui.port=5100 --jars /usr/share/java/mysql-connector-java-9.1.0.jar {script_1}",
+    dag=dag,
+)
+
+end = DummyOperator(
+    task_id='end',
+    dag=dag,
+)
+
+
+# Set dependencies if needed
+start >> task1 >> task2 >> end
